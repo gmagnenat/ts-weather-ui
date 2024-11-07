3df195b6fba796160ea02f3f6c88d1b6275665bc
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-07 12:56:35 +0100

    Init repo with latest changes

---
+++ src/backend/location.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import type { AxiosStatic } from "axios";
+
+const locationInfoSchema = z.object({
+  lat: z.string(),
+  lon: z.string(),
+  display_name: z.string(),
+});
+
+export type LocationInfo = z.infer<typeof locationInfoSchema>;
+
+export async function fetchLocationData(
+  axios: AxiosStatic,
+  apiUrl: string,
+  locationName: string,
+  apiKey?: string
+): Promise<LocationInfo> {
+  const options = {
+    method: "GET",
+    url: apiUrl,
+    params: {
+      q: locationName,
+      api_key: apiKey,
+    },
+  };
+
+  const response = await axios.request(options);
+  if (response.status === 200) {
+    try {
+      return locationInfoSchema.parse(response.data[0]);
+    } catch (err) {
+      console.log(err);
+      throw new Error(
+        `Unable to find location information for ${locationName}`
+      );
+    }
+  } else {
+    throw new Error("Failed to fetch location data");
+  }
+}
